9662200c19a73ebdc6f6946dd2c442ec8f6a8c43
Sean Andersen <smandersen13@gmail.com>
2018-02-17 12:45:05 -0500

    added additional question

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,17 +15,26 @@ connection.connect()
 
 
 
-inquirer.prompt([{
-name: "name",
-type: "list",
-message: "Which product would you like to buy?",
-choices: ["Fire TV Stick", "Beats Headphones", "Computer Monitor", "Gym Bag", "Soccer Training Pants", "Running Shoes", "Real Estate Book", "Desserts Book", "Action Figures", "Legos"]
-}]).then(function(response) {
-  console.log(response)
-
-  connection.query("SELECT * FROM products", function(error,results) {
-    if(error) throw(error)
-
+inquirer.prompt([
+{
+  name: "product",
+  type: "list",
+  message: "Which product would you like to buy?",
+  choices: ["Fire TV Stick", "Beats Headphones", "Computer Monitor", "Gym Bag", "Soccer Training Pants", "Running Shoes", "Real Estate Book", "Desserts Book", "Action Figures", "Legos"]
+},
+{
+  name: "quantity",
+  type: "input",
+  message: "how much of" +
+}]).then(function(productResponse) {
+  console.log(productResponse)
+  inquirer.prompt([{
+    name: "quantity",
+    type: "input",
+    message: "How many" + productResponse + "do you want?"
+  }]).then(function(quantityResponse) {
+    console.log(quantityResponse)
+    })
   })
   connection.end()
 
