b6dde83fe8966d2f0b9f06bfc2154432a1637f1c
Sean Andersen <smandersen13@gmail.com>
2018-02-16 20:44:47 -0500

    additional table entries

--- Amazon-LikeStorefront-Products.sql
+++ Amazon-LikeStorefront-Products.sql
@@ -4,5 +4,8 @@ USE bamazon;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Fire TV Stick','Electronics', 39.99, 25);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Gym Bag', 'Sports & Fitness Clothing', 23.49, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Soccer Training Pants', 'Sports & Fitness Clothing', 19.99, 15)
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Soccer Training Pants', 'Sports & Fitness Clothing', 19.99, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Running Shoes', 'Sports & Fitness Clothing', 79.99, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Real Estate Book', 'Books & Audible', 15.99, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Desserts Book', 'Books & Audible', 11.99, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (
